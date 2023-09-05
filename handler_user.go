e6c1397bd83846cd26cc6d2f56ae152d1766f731
Ian <tanyixiong27@gmail.com>
2023-09-06 11:08:43 +1200

    update

--- handler_user.go
+++ handler_user.go
@@ -46,12 +46,12 @@ func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, 
 
 func (apiCfg *apiConfig) handlerGetPostsForUser(w http.ResponseWriter, r *http.Request, user database.User) {
 	posts, err := apiCfg.DB.GetPostsForUser(r.Context(), database.GetPostsForUserParams{
-		user.ID,
-		10,
+		UserID: user.ID,
+		Limit:  10,
 	})
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Couldn't get posts: %v", err))
 		return
 	}
-
+	respondWithJSON(w, 200, databasePostsToPosts(posts))
 }
